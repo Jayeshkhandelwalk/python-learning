65a78c07927f1c624bf8c5269a3c16103f0bc885
Jayesh Khandelwal <98072547+Jayeshkhandelwalk@users.noreply.github.com>
2026-09-26 23:47:29 +0530

    Add files via upload

---
+++ day13pr.py
@@ -0,0 +1,13 @@
+a=input("enter your name")
+print(a.strip(" "))
+print(a.upper())
+print(a.lower)
+print(len(a))
+print(a.replace(" ","-"))
+print(a.capitalize())
+print(a.count("e"))
+print(a.endswith('a'))
+print(a.find("d"))
+
+
+ #this will remove the a from the right side of the string
